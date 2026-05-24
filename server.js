d16cbedcc1c86fcf972eca776c94a526840b1b17
Aviator-Predictor-Pro-v2 <zashkim888@gmail.com>
2026-05-25 01:27:14 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/sounds', express.static(path.join(__dirname, 'sounds')));
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
